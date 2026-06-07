f928362eceebd59e3cc1656f6dbc34fe670e1172
Vitalii Hoshuliak <goshulyakvitalik@gmail.com>
2026-06-07 21:00:10 +0100

    20/59

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const mainImage = document.querySelector('.main-image');
+
+gallery.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  // Якщо клік по <img>, беремо його батьківський <a>
+  let target = event.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
+
+  // Якщо клік не по <a> — нічого не робимо
+  if (target.tagName !== 'A') {
+    return;
+  }
+
+  const newSrc = target.getAttribute('href');
+
+  mainImage.setAttribute('src', newSrc);
+});
